75094de92b11af001bf95b7f304eb9ed7b9e0481
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-03-25 13:22:01 +0900

    Create Pointer_lec_10.08.c

---
+++ Pointer_lec_10.08.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+
+/* 10.8 두 개의 포인터로 배열을 함수에게 전달해주는 방법 */
+
+double average(double* start, double* end);
+
+int main()
+{
+	double arr1[5] = { 10,13,12,7,8 };
+
+	printf("Avg = %f\n", average(arr1, arr1 + 5));
+
+	return 0;
+}
+
+double average(double* start, double* end) {
+	
+	int count = end  - start;
+	double avg = 0.0;
+	while (start < end) {
+		avg += *start++;
+		//count++;
+	}
+	avg /= (double)count;
+
+	return avg;
+}
