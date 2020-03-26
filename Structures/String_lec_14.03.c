112a634a1b03fba3f83e7b4828ccf2363ff92d6d
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-03-26 22:17:09 +0900

    Struct

---
+++ Structures/String_lec_14.03.c
@@ -0,0 +1,98 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	struct Aligned { // 구조체를 변수선언할 때는 메모리에 할당x
+		int a;  //4
+		float b; //4
+		double c;//8
+	};
+
+	struct Aligned a1, a2;
+
+	printf("struct Aligned a1\n");
+	printf("Sizeof %zd\n", sizeof(struct Aligned)); // 16
+	printf("%lld\n", (long long)&a1); // 주소 0
+	printf("%lld\n", (long long)&a1.a); // 주소 0
+	printf("%lld\n", (long long)&a1.b); // 주소 4
+	printf("%lld\n", (long long)&a1.c); // 주소 8
+
+	printf("struct Aligned a1\n");
+	printf("Sizeof %zd\n", sizeof(struct Aligned));
+	printf("%lld\n", (long long)&a2);
+	printf("%lld\n", (long long)&a2.a);
+	printf("%lld\n", (long long)&a2.b);
+	printf("%lld\n", (long long)&a2.c);
+
+
+	/* ----------padding (struct member alignment)---------- 
+				1 word: 4 bytes in x86, 8 bytes in x64
+
+				Memorry <--> CPU 데이터를 서로 보낼때,
+				데이터를 묶어서 보낸다. (word 만큼씩)
+	*/
+
+	struct Padded1 {
+		char a; // 1 byte
+		float b; // 4
+		double c; // 8
+	}; // 1 + 4 + 8 = 13
+
+	/*With padding
+	0 1 2 3 |4 5 6 7 |8 9 10 11 12 13 14 15 16
+	|char a |float b|double c
+	char는 1byte인데 컴파일러가 3byte를 추가해 놓은다.(padding)
+	(word 단위로 정보를 한번에 보내기 위해서)
+	*/
+
+	struct Padded1 p1;
+	printf("struct Aligned p1\n");
+	printf("Sizeof %zd\n", sizeof(p1)); /* 16btye */
+	printf("%lld\n", (long long)&p1); // 주소 0
+	printf("%lld\n", (long long)&p1.a); // 주소 0
+	printf("%lld\n", (long long)&p1.b); // 주소 4
+	printf("%lld\n", (long long)&p1.c); // 주소 8
+
+	struct Padded2 {
+		float a; // 4
+		double b; // 8
+		char c; // 1
+	};
+
+	/*
+	|0 1 2 3 4 5 6 7|8 9 10 11 12 13 14 15|16 17 18 19 20 21 22 23|24
+	|float a		|double b			  | char c				  |
+	(padding 이 float와 char 둘다 들어감.)
+	*/
+
+
+	struct Padded2 p2;
+	printf("struct Aligned p2\n");
+	printf("Sizeof %zd\n", sizeof(p2)); /* 16btye */
+	printf("%lld\n", (long long)&p2); // 주소 0
+	printf("%lld\n", (long long)&p2.a); // 주소 0
+	printf("%lld\n", (long long)&p2.b); // 주소 8
+	printf("%lld\n", (long long)&p2.c); // 주소 6
+
+	struct Person {
+		char name[41];
+		int age;
+		float height;
+	};
+
+	struct Person mommy;
+	printf("struct Person\n");
+	printf("Sizeof %zd\n", sizeof(mommy)); // 52
+	printf("%lld\n", (long long)&mommy); // 주소 0
+	printf("%lld\n", (long long)&mommy.name); // 주소 0
+	printf("%lld\n", (long long)&mommy.age); // 주소 44
+	printf("%lld\n", (long long)&mommy.height); // 주소 52
+
+
+	/*----------구조체 배열----------*/
+	struct Person f[4];
+
+	printf("Sizeof a structure array %zd\n", sizeof(f)); // 208 = 52 * 4
+
+	return 0;
+}
---
+++ Structures/Struct_lec_14.02.c
@@ -0,0 +1,112 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 41
+
+struct person { // Person is a tag of structure
+
+	char name[MAX]; // member
+	int age; /* member ,구조체 안에서 맴버를 선언해줄때는 메모리를 갖지 않는다. 
+			 구조체 변수가 선언이 될 때, 메모리를 갖는다. */
+	float height; // member
+};
+
+int main()
+{
+	int flag;
+
+	/* ----------Structure variable(구조체 변수 접근)---------- */
+
+	struct person genie; /* struct tag(이름) 변수 
+						 변수를 선언하는 순간 구조체 멤버들이 메모리에 할당된다.*/
+
+	/* dot(.) is structure membership operator (member access operator, member operator)*/
+	// 구조체 멤버 접근
+	strcpy(genie.name, "Will Smith");
+	genie.age = 1000;
+
+	flag = scanf("%f", &genie.height);
+	printf("%f\n", genie.height);
+
+
+
+	/* ----------Initializtion (구조체 초기화)---------- */
+
+	struct person princess = { "Naomi Scott", 18, 160.0f };
+
+	struct person princess2 = {
+		"Naomi Scott",
+		18,
+		160.0f
+	};
+
+	strcpy(princess.name, "Naomi Scott");
+	princess.age = 18;
+	princess.height = 160.0f;
+
+
+	/* ----------Designated initializers(지정해서 초기화)---------- */
+
+	struct person beauty = {
+		.age = 19,
+		.name = "Bell",
+		.height = 150.0f
+	// 구조체 멤버 순서와 달리 지정해도 가능. 
+	};
+
+
+	/* ----------Pointer to a strcture variable(구조체 변수에 대한 포인터)---------- */
+
+	struct person* someone;
+	someone = &genie;
+
+	// Indirect member(ship) operator (or structure pointer operator)
+
+	// 포인터가 구조체 접근할 때는 dot 대신에 arrow에 operator를 사용한다.
+	someone->age = 1001;
+	printf("%s %d\n", someone->name, (*someone).age);
+
+
+	/* ----------Structure declarations in a function(함수 안에서 구조체 선언)---------- */
+
+	struct book {
+		char title[MAX];
+		float price;
+	};
+
+
+	/* ----------No tag----------*/
+
+	struct {
+		char farm[MAX];
+		float price;
+	} apple, apple2; // 선언
+
+	strcpy(apple.farm, "Trade Joe");
+	apple.price = 1.2f;
+
+	strcpy(apple2.farm, "Safeway");
+	apple2.price = 5.6f;
+
+
+	/* ----------typedef and structure(구조체가 사용자가 정의한 새로운 자료형 처럼 사용하기)---------- */
+
+	typedef struct person my_person;
+
+	my_person p3; // my_person 자료형에 대한 p3변수
+	
+	typedef struct person person;
+
+	person p4;
+	
+	typedef struct {
+		char name[MAX];
+		char hobby[MAX];
+	}friend;
+
+	friend f4;
+	
+
+	return 0;
+}
