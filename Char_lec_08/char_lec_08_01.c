431c5ad0fc127062ba050fe1f1b78ed5692731d4
kyesungil <41607872+kyesungil@users.noreply.github.com>
2020-03-30 14:02:28 +0900

    Create char_lec_08_01.c

---
+++ Char_lec_08/char_lec_08_01.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNIGS
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char c;
+
+	/* buffer가 중간에 없으면, 입력을 한 그대로 바로 출력을 한다.*/
+	//while ((c = _getche()) != '.') /* _getche() buffer를 통하지 않고 작동. */
+	//	putchar(c);
+
+
+	/* buffer가 있다면, 입력할 때마다 한 글자 씩 버퍼로 이동, 버퍼에 담긴 내용이 통째로 전달*/
+	while ((c = getchar()) != '.')
+		putchar(c);
+	return 0;
+}
